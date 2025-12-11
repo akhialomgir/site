e62871e432eee1435b28e7952b5eaef185e4d0fb
akhia <akhialomgir362856@gmail.com>
2025-12-11 20:59:50 +0800

    feat: components

--- server.ts
+++ server.ts
@@ -1,4 +1,23 @@
-import { createServer } from 'http';
-import { readFileSync } from 'fs';
-const html = readFileSync('index.html');
-createServer((_, res) => res.end(html)).listen(3000);
+import { readdirSync, readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+
+const comps: Record<string, string> = {};
+const dir = "./components";
+
+for (const file of readdirSync(dir)) {
+  if (!file.endsWith(".html")) continue;
+  const name = file.replace(".html", "");
+  comps[name] = readFileSync(join(dir, file), "utf8").trim();
+}
+
+for (const file of readdirSync(".")) {
+  if (!file.endsWith(".html")) continue;
+
+  let content = readFileSync(file, "utf8");
+  for (const name in comps) {
+    const tag = new RegExp(`<${name}\\s*/>`, "g");
+    content = content.replace(tag, comps[name]);
+  }
+
+  writeFileSync(file, content);
+}
